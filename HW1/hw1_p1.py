bbb8d118077d9b7d1317b38312c47ca1e224b866
kgandhi09 <kushal009gandhi@gmail.com>
2020-03-30 02:45:30 -0400

    HW1 problem 1 solved

---
+++ HW1/hw1_p1.py
@@ -0,0 +1,23 @@
+#Problem 1: List Sorting
+
+gods = ["Notus", "Momus", "Nereus", "Glaucus", "Heracles", "Eurus", "Aether", "Phosphorous", "Zelus", "Tartarus"]
+dummy = ["Hello", "Hella"]
+
+def sortList(list):
+    for i in range(len(list)-1):
+        for j in range(len(list)-1):
+            if list[j][0] > list[j+1][0]:
+                list[j], list[j+1] = list[j+1], list[j]
+            #if the first letter of the strings are same, then jump to next letter
+            if list[j][0] == list[j+1][0]:
+                ind = 1
+                while True:
+                    ind = ind + 1
+                    list[j], list[j+1] = list[j+1], list[j]
+                    if (list[j][ind] != list[j+1][ind]):
+                        break
+                
+    print(list)
+
+sortList(gods)
+sortList(dummy)
