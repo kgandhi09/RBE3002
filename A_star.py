e7fdf9aaeb17d1b4e28de407472970cb2fded106
kgandhi09 <kushal009gandhi@gmail.com>
2020-04-29 05:24:25 -0400

    A* working

--- A_star.py
+++ A_star.py
@@ -1,21 +1,7 @@
-def a_star(self, mapdata, start, goal):
-        ### REQUIRED CREDIT
-        print "In a star"
-        # rospy.loginfo("Executing A*") # from (%d,%d) to (%d,%d)" % (start[0], start[1], goal[0], goal[1]))
-        # start_index = PathPlanner().grid_to_index(mapdata, start[0], start[1])
-        # goal_index = PathPlanner().grid_to_index(mapdata, goal[0], goal[1])
-
+def convert_to_tree(self, mapdata, start, goal):
+        #making a tree from graph in dictionary form
         tree = {}
 
-        start_index_tree = self.grid_to_index(mapdata, start[0], start[1]) 
-        goal_index_tree = self.grid_to_index(mapdata, goal[0], goal[1])
-
-        path = []
-
-        visited = []
-
-
-        #making a tree from graph in dictionary form
         for i in range(0,mapdata.info.width):
             for j in range(0,mapdata.info.height):
                 
@@ -39,46 +25,52 @@ def a_star(self, mapdata, start, goal):
                     fn_data = [heuristic[k], cost[k]]
                     child_heur[tuple(neighbors[k])] = fn_data
                     
-                tree[index] = child_heur          
+                tree[index] = child_heur
+        return tree
 
-                # print [j,i], index, tree[index]
 
-        temp_index_tree = start_index_tree
+def a_star(self, mapdata, start, goal):
+    ### REQUIRED CREDIT
+
+    start_index_tree = self.grid_to_index(mapdata, start[0], start[1]) 
+
+    tree = self.convert_to_tree(mapdata, start, goal)                  
+
+    temp_index_tree = start_index_tree
+    
+    fn_child_node = []
+
+    track_node = []
+
+    # if (mapdata.data[self.grid_to_index(mapdata, goal[0], goal[1])]) == 100:
+    #     print 'invalid goal position'
+
+    for i in range(len(tree)):
+        if i == temp_index_tree:
+            child_nodes = tree[i]
+            count = 0
+            # print child_nodes
+            for node in child_nodes:
+                fn = child_nodes[node][0] + child_nodes[node][1]        #f(n) = g(n) + h(n)
+                track_node.append(child_nodes[node][0])
+                fn_child_node.append(fn)
+                
+            min_index = fn_child_node.index(min(fn_child_node))
+            min_child_index = track_node[min_index]
         
-        fn_child_node = []
-
-        track_node = []
-
-        for i in tree:
-            if i == temp_index_tree:
-                child_nodes = tree[i]
-                count = 0
-                # print child_nodes
-                for node in child_nodes:
-                    fn = child_nodes[node][0] + child_nodes[node][1]        #f(n) = g(n) + h(n)
-                    track_node.append(child_nodes[node][0])
-                    fn_child_node.append(fn)
+            for node in child_nodes:
+                if min_child_index == child_nodes[node][0] and count == 0:
                     
+                    next_node = node
+                    self.path.append(next_node)
+                    temp_index_tree = self.grid_to_index(mapdata, next_node[0], next_node[1])
+                    
+                    if self.path != [] and temp_index_tree < start_index_tree:
+                        print next_node[0], next_node[1], goal[0], goal[1]
+                        if next_node[0] != goal[0] and next_node[1] != goal[1]:
+                            self.a_star(mapdata, next_node, goal)
+                    count = 1
                     
                 
-                min_index = fn_child_node.index(min(fn_child_node))
-                min_child_index = track_node[min_index]
-                # print min_child_index
-                
-                for node in child_nodes:
-                    if min_child_index == child_nodes[node][0] and count == 0:
-                        path.append(node)
-                        next_node = node
-                        
-                        # if next_node[0] == goal[0] and next_node[1] == goal[1]: 
-                        #     return path
-                        # else:
-                        temp_index_tree = self.grid_to_index(mapdata, next_node[0], next_node[1])
-                        print next_node, temp_index_tree, start_index_tree
-                        if temp_index_tree < start_index_tree:
-                            i = 0
-                            continue
-                            print 'after continue'
-                        count = 1
-                        
-        return path
+
+    return self.path
